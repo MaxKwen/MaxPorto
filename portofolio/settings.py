7ec5adf2148e6c768c5676b1b50637df8f8c20f4
Maxi <maximus.quinn@gmail.com>
2026-08-31 23:25:05 +0700

    Tutorial 1 with no change in HTML and CSS

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -55,12 +55,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'MaxPorto.urls'
+ROOT_URLCONF = 'portofolio.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,7 +72,10 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'MaxPorto.wsgi.application'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+WSGI_APPLICATION = 'portofolio.wsgi.application'
 
 
 # Database
